2186eee90d2674d02342ac7c432365ad4ff93f92
Austinbb <austinbaumbach2@gmail.com>
2022-05-06 11:34:44 -0700

    lift state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import BoxColors from './components/BoxColors';
 
 
 function App() {
+
+  const [boxes, setBoxes] = useState([]);
   
   return (
   <div className='App'>
-    <BoxColors />
+    <BoxColors boxes = { boxes } setBoxes = { setBoxes } />
+
+    <div style={{ display: "flex" }}>
+      {
+        boxes.map((color, i) => {
+        return <div key={i} style={{ 
+          backgroundColor: color, 
+          width: 200, 
+          height: 200, 
+          marginRight: 30 
+          }}></div>
+          
+        } )
+      }
+    </div>
   </div>
   );
 }
--- src/components/BoxColors.jsx
+++ src/components/BoxColors.jsx
@@ -2,28 +2,22 @@ import React, { useState } from 'react'
 
 const BoxColors = (props) => {
     const [boxColor, setBoxColor] = useState("");
-    const [boxes, setBoxes] = useState([]);
+    
 
     const newBox = (e) => {
         e.preventDefault();
-        setBoxes([...boxes, boxColor]);  
+        props.setBoxes([...props.boxes, boxColor]);
+        setBoxColor("");
     }
 
     return (
         <div className='App'>
             <form onSubmit={ newBox } className='Form'>
                 <label>Color: </label>
-                <input onChange={(e) => { setBoxColor(e.target.value) }} type="text" />
+                <input onChange={(e) => { setBoxColor(e.target.value) }} type="text" value={ boxColor }/>
                 <button>Add</button>
             </form>
 
-            <div style={{ display: "flex" }}>
-            {
-                boxes.map((color, i) => {
-                return <div key={i} style={{ backgroundColor: color, width: 200, height: 200, marginRight: 30 }}></div>
-                } )
-            }
-            </div>
         </div>
     )
 }
